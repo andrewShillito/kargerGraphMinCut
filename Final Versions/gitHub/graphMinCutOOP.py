ebfd31b6901a6287316ed2306ed5473441ebf3f2
andrewShillito <34523587+A-S-8@users.noreply.github.com>
2018-08-05 20:34:35 -0400

    build classes and graph constructor.  currently constructor does not work correctly - issue in checking for duplicate edges

--- Final Versions/gitHub/graphMinCutOOP.py
+++ Final Versions/gitHub/graphMinCutOOP.py
@@ -6,65 +6,71 @@ Created on Tue Jul 31 19:34:35 2018
 @author: Andrew Shillito
 
 Current issues:
-    no testing for node membership when adding edges - will fix
+    no testing for node membership when adding edges
     
-    having node.getEdges() makes graph dict redundant
-    if I remove getEdges() I can accomplish the same thing
-    by indexing into the graph[node] which will make more sense
+    Node.nodeList as class wide var doesn't allow for multiple graph
+    constructions in single run unless it is cleared before each
+    which could be done
+    
+    the checking for duplicate edges is incorrect in graph builder
+    (and unfinished)
+    
+    currently the graph keys are node objects which is creating problems
+    when trying to index into the graph while adding edges in constructor
+    
 """
 
-import random, copy, os, re
+import random, copy, os, re, pdb
 
 class Graph(object):
     def __init__(self): 
-#assumes graph is pre-constructed dict (nodes/edges) adjacency list
-        self.nodes = []
         self.edges = []
         self.graph = {}
         
     def getNodes(self):
-        return self.nodes[:]
+        return list(self.graph.keys())
     
     def getEdges(self):
         return self.edges[:]
-    
-#    def getGraphCopy(self):
-#        return copy.deepcopy(self.graph)
-    
+
     def combineNodes(self, node, otherNode):
         return None
     
-    def contractNodes(self, node, otherNode):
+    def contractNodes(self, edge):
         return None
     
-    def selectEdge(self, edges):
-        randEdge = random.sample(edges, 1)[0]
+    def selectEdge(self):
+        randEdge = random.sample(self.edges, 1)[0]
         return randEdge
     
     def removeSelfLoops(self, node, otherNode):
         return None
     
     def addNode(self, node):
-        self.nodes.append(node)
+        self.graph[node]=[]
         return None
     
     def removeNode(self, node):
-        self.nodes.remove(node) #this is O(n)
+        del self.graph[node]
+        try:
+            del self.graph[node]
+        except KeyError:
+            print("No such node in Graph")
         return None
     
     def addEdge(self, node, otherNode):
         newEdge = Edge(node, otherNode)
-        node.edges.append(newEdge)
-        otherNode.edges.append(newEdge)
+        self.graph[node].append(newEdge)
+        self.graph[otherNode].append(newEdge)
         self.edges.append(newEdge)
-    #appends the same edge refernced all three places
+    #appends the same edge referenced all three places
         return None
     
     def removeEdge(self, edge):
         try:
             self.edges.remove(edge)
-            edge.getSrc().edges.remove(edge)
-            edge.getDest().edges.remove(edge)
+            self.graph[edge.getSrc()].remove(edge)
+            self.graph[edge.getDest()].remove(edge)
         except ValueError:
             print("Edge not present")
             pass
@@ -72,9 +78,9 @@ class Graph(object):
     
     def __str__(self):
         ansString = ''
-        for node in self.nodes:
+        for node in list(self.graph.keys()):
             ansString+= node.getName()+" --> "
-            for edge in node.getEdges():
+            for edge in self.graph[node]:
                 temp = edge.getNodes()
                 for i in temp:
                     if i!=node:
@@ -85,15 +91,12 @@ class Graph(object):
     
 
 class Node(object):
+    
+    nodeList = []
+    
     def __init__(self, name): #edges a list of edges??
         self.name = name
-#        self.edges = []
-        
-#    def getEdges(self):
-#        return self.edges[:]
-    
-    def removeEdge(self, edge):
-        return None
+        Node.nodeList.append(name)
     
     def getName(self):
         return self.name
@@ -112,48 +115,63 @@ class Edge(object):
     def getDest(self):
         return self.head
     
-n1 = Node('1')
-n2 = Node('2')
-n3 = Node('3')
-graph = Graph()
-graph.addNode(n1)
-graph.addNode(n2)
-graph.addNode(n3)
-graph.addEdge(n1, n2)
-graph.addEdge(n1, n3)
-graph.addEdge(n3, n2)
-graph.addEdge(n3, n1)
+#n1 = Node('1')
+#n2 = Node('2')
+#n3 = Node('3')
+#graph = Graph()
+#graph.addNode(n1)
+#graph.addNode(n2)
+#graph.addNode(n3)
+#graph.addEdge(n1, n2)
+#graph.addEdge(n1, n3)
+#graph.addEdge(n3, n2)
+#graph.addEdge(n3, n1)
 #graph.removeEdge(graph.getEdges()[0])
 
 def constructGraph():
-    directory = os.getcwd()+"\\testCases"
-    tests = []
-    outputs = []
+    directory = os.path.abspath("..")+"\\testCases"
     graphs = []
-    y = 0
-    testFiles = [i for i in os.listdir(directory) if "Output" not in i]
-    testFiles = testFiles[:-1]
+    outputs = []
+    testFiles = [i for i in os.listdir(directory) if "Output" not in i and ".rtf" not in i]
+    print(testFiles)
     outputFiles = [j for j in os.listdir(directory) if "Output" in j]
+    print(outputFiles)
     for x in range(len(testFiles)):
-        graph = []
-        temp = testFileRead(testFiles[x], graph)
-        outputs.append(outputFileRead(outputFiles[x]))
+        graph = Graph()
+        testFileRead(directory+"\\"+testFiles[x], graph)
+        outputs.append(outputFileRead(directory+"\\"+outputFiles[x]))
+        graphs.append(graph)
+        Node.nodeList = []
     return graphs, outputs
 
 def testFileRead(fileName, graph):
     file = open(fileName)
+#    pdb.set_trace()
     for line in file:
         temp = re.split(r"\D", line)
         while "" in temp:
             temp.remove('')
-        node = Node(temp[0])
-        graph.addNode(node)
-        for edge in temp[1:]:
-            pass #left off here - fixing node and graph implementation-
-            #graph now has self.graph which is a dict node-->edges
-            #must change node class, Graph.self.graph, graph.addNode graph.addEdge
-            #graph.removeEdge, graph.removeNode, and graph.__str__
+        if temp[0] not in Node.nodeList:    
+            node = Node(temp[0])
+            graph.addNode(node)
+        else:
+            for i in list(graph.graph.keys()):
+                if i.name==temp[0]:
+                    node = i
+                    break
+        for destNode in temp[1:]:
+            if destNode not in Node.nodeList:
+                otherNode = Node(destNode)
+                graph.addNode(otherNode)
+                graph.addEdge(node, otherNode)
+            else:
+                for j in list(graph.graph.keys()):
+                    if j.name==destNode:
+                        otherNode=j
+                        break
+#                if 
     file.close()
+    return None
     
 def outputFileRead(fileName):
     file = open(fileName)
